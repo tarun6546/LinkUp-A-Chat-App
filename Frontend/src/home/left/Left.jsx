e74195fb4965bc30bcb0f99b5ada3b7284c98c47
tarun6546 <tarunvarshney2112@gmail.com>
2025-03-24 23:55:31 +0530

    add react-icons dependency and update Search component layout

--- Frontend/src/home/left/Left.jsx
+++ Frontend/src/home/left/Left.jsx
@@ -4,7 +4,6 @@ import Search from './Search'
 export default function left() {
   return (
     <div className='w-[30%] border  border-white bg-black text-white'>
-        Left
         <Search></Search>
     </div>
   )
