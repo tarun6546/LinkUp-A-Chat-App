c16e4173bf6d76048fa7bbb03b3cedc270bae688
tarun6546 <tarunvarshney2112@gmail.com>
2025-03-25 00:23:46 +0530

    add Users component and integrate user list in Left component

--- Frontend/src/home/left/Left.jsx
+++ Frontend/src/home/left/Left.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Search from './Search'
+import User from './User'
 
 export default function left() {
   return (
     <div className='w-[30%] border  border-white bg-black text-white'>
+      <h1 className='font-bold text-3xl p-2 px-11 align-top'>Chats</h1>
         <Search></Search>
+        <hr></hr>
+        <hr></hr>
+        <User></User>
     </div>
   )
 }
