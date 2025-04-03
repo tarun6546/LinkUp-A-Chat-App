9a5e56a312b8ca877a61f1effade61749666cc6f
tarun6546 <tarunvarshney2112@gmail.com>
2025-04-03 23:44:07 +0530

    chaging

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
-import Left from  "./home/left/left.jsx"
+import Left from "./home/left/left.jsx"  // Changed to capital L
 import Right from "./home/right/Right.jsx"
 
 const App = () => {
   return (
     <>
       <div className="flex h-screen">
-        <Left></Left>
-        <Right></Right>
+        <Left />   {/* Changed to self-closing tag */}
+        <Right />  {/* Changed to self-closing tag */}
       </div>
     </>
   )
+  ?efefer
 }
 
 export default App
